c7ace2f220ecbd04bfe285e6f7164114f56ad0c8
whalebro <steven.chou.second@gmail.com>
2019-12-14 17:37:09 +0800

    init V2

--- App.js
+++ App.js
@@ -1,7 +1,19 @@
 import React from 'react';
-
-import PlacesNavigator from './navigation/PlacesNavigator';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return <PlacesNavigator />;
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
