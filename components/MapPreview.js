888e5e860720ee5a1f7ad6f69a608b1b332651ba
whalebro <steven.chou.second@gmail.com>
2019-12-16 19:34:08 +0800

    Displaying an Interactive Map

--- components/MapPreview.js
+++ components/MapPreview.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import { TouchableOpacity, Image, StyleSheet } from 'react-native';
 
 import ENV from '../env';
 
@@ -13,17 +13,20 @@ const MapPreview = props => {
       props.location.lng
     }&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:A%7C${
       props.location.lat
-    },${props.location.lng}&key=${ENV.googleApiKey}`;
+    },${props.location.lng}&key=${ENV().googleApiKey}`;
   }
 
   return (
-    <View style={{ ...styles.mapPreview, ...props.style }}>
+    <TouchableOpacity
+      onPress={props.onPress}
+      style={{ ...styles.mapPreview, ...props.style }}
+    >
       {props.location ? (
         <Image style={styles.mapImage} source={{ uri: imagePreviewUrl }} />
       ) : (
         props.children
       )}
-    </View>
+    </TouchableOpacity>
   );
 };
 
--- env.js
+++ env.js
@@ -1,5 +1,23 @@
-const vars = {
-  googleApiKey: 'AIzaSyAc4mNGdPzTNZuR3qCnz62W5OQ4PXYlQFU'
+// const vars = {
+//   googleApiKey: 'AIzaSyAc4mNGdPzTNZuR3qCnz62W5OQ4PXYlQFU'
+// };
+
+// export default vars;
+
+const variables = {
+  development: {
+    googleApiKey: 'AIzaSyAc4mNGdPzTNZuR3qCnz62W5OQ4PXYlQFU'
+  },
+  production: {
+    googleApiKey: 'xyz'
+  }
 };
 
-export default vars;
+const getEnvVariables = () => {
+  if (__DEV__) {
+    return variables.development; // return this if in development mode
+  }
+  return variables.production; // otherwise, return this
+};
+
+export default getEnvVariables; // export a reference to the function
